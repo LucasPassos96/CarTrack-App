d545b87a6548f5cfecfeecde2e8d0afb01a93b90
LucasBinho <lucas_passos@msn.com>
2025-05-10 09:04:38 -0300

    iniciando projeto; importando packages necessarios; configurando tailwind; criacao de header;

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return(
+        <header className="mb-6 text-xl font-bold bg-[#001e2e] text-white p-3 pl-4">
+            Lucas passos
+        </header>
+    );
+}
